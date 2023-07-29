1a2de8471b20b06920e611755d9c04ff842a2da0
Seywen-Spec <Seywen.Jad101@gmail.com>
2023-07-29 17:35:53 +0300

    TAO5 v1.0

---
+++ C1 - Supervised Machine Learning - Regression and Classification/week2/Optional Labs/TAO6.py
@@ -0,0 +1,5 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from lab_utils_multi import zscore_normalize_features, run_gradient_descent_feng
+np.set_printoptions(precision=2)  # reduced display precision on numpy arrays
+
